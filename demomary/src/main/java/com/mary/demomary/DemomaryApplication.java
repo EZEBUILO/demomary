24e99e8061b8862e2b58d15a85326e02613ddf9f
kennedy <ezebuilokennedy@gmail.com>
2023-07-18 12:19:37 +0100

    first commit for demomary evan

---
+++ demomary/src/main/java/com/mary/demomary/DemomaryApplication.java
@@ -0,0 +1,13 @@
+package com.mary.demomary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemomaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemomaryApplication.class, args);
+	}
+
+}
